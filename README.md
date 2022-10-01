ea8dc80fea4738ffd5d32c7ae01db7df42571cb6
Vladimir Botka <vbotka@gmail.com>
2022-10-01 02:44:34 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# mos-math
+[![](https://img.shields.io/pypi/v/mos-math?color=green)](https://pypi.org/project/mos-math/)[![license](https://img.shields.io/badge/license-BSD-red.svg)](https://www.freebsd.org/doc/en/articles/bsdl-gpl/article.html)
+
+
+MOS mathematics for doctoral thesis
+
+[Electrophysical properties of the MOS structure with implanted bulk](https://github.com/vbotka/thesis)
---
+++ mos_math/basics.py
@@ -0,0 +1,38 @@
+# All rights reserved (c) 2022, Vladimir Botka <vbotka@gmail.com>
+# Simplified BSD License, https://opensource.org/licenses/BSD-2-Clause
+
+from hamming_digital_filters import gradient as hdfg
+import numpy
+
+k = 1.38e-23
+T = 300
+q = 1.602e-19
+ni = 1.45e16
+
+
+def clf_from_fi(fn, vg, fnorm, gtype, f_critical=0.1, no_filter_points=7):
+    ''' Normalized LF C-V of MOS structure from the normalized surface potential '''
+    f = [(1.0 - x) * fnorm for x in fn]
+    dv = vg[1] - vg[0]
+    if gtype == "hamming":
+        dfdv = hdfg.gradient_nr(f, dv, f_critical, no_filter_points)
+    else:
+        dfdv = numpy.gradient(f, dv)
+    return [(1.0 - x) for x in dfdv]
+
+
+def _dit(clf, chf, cox):
+    return ((clf * cox) / (1.0 - clf) - (chf * cox) / (1.0 - chf)) / q
+
+
+def dit_from_clf_chf(clf, chf, cox):
+    ''' Density of interface traps from LF and HF C-V '''
+    return [_dit(clfi, chfi, cox) for clfi, chfi in zip(clf, chf)]
+
+
+def eband_of_dit(fn, fnorm, bulk, stype):
+    ''' Position of Dit in the forbiden band '''
+    ff = (k * T / q) * numpy.log(bulk / ni)
+    if stype == 'P':
+        ff = - ff
+    return [((1.0 - f) * fnorm + 0.56 + ff) for f in fn]
